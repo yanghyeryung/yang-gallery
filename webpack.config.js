5e3b301c3efc62c0435c384942a072febf1cb980
양혜령 <hyeryung.yang@bankwareglobal.com>
2020-06-30 21:57:09 +0900

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: 'development',
     entry: {
-        main: './src'
+        main: './src/index.js'
     },
     output: {
         filename: '[name].js',
